7b9024d7647dbdc3e88d9fa35d14c872a035f878
ra220599 <b220599@dac.unicamp.br>
2023-03-14 22:53:09 -0300

    1ª Atualização

---
+++ seguradora/Cliente.java
@@ -0,0 +1,26 @@
+
+public class Cliente {
+	private String nome;
+	private String cpf;
+	private String dataNascimento;
+	private int idade;
+	private String endereco;
+	
+	// Construtor
+	public Cliente(String nome, String cpf, String dataNascimento, int idade, String endereco) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.dataNascimento = dataNascimento;
+		this.idade = idade;
+		this.endereco = endereco;
+	}
+	
+	//Getters e setters
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
